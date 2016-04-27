c1f8a9e8ee0f225b3e2f683c9bc63e8123b0b01e
Rodney Weis <rodweis@gmail.com>
2016-04-27 12:39:37 -0600

    Added panels for Ship Details, Warehouse, Price List and City List

--- js/taipan.js
+++ js/taipan.js
@@ -8,14 +8,27 @@ function readyFn(jQuery) {
 /* Global Data Initialization 
 	TODO:  Import JSON data from source repo
 */
-var cities = ['Hong Kong', 'Shanghai', 'Nagasaki', 'Saigon', 'Manila', 'Singapore', 'Batavia'];
 
-var merchName = ['General Cargo', 'Arms', 'Silk', 'Contraband'];
+var warehouseCapacity = 10000,
+	shipCapacity = 100,
+	shipGuns = 5;
+	shipHealth = 100;
 
-var merchBasePrice = [15, 50, 750, 9000];
+var cities = ['Hong Kong', 'Shanghai', 'Nagasaki', 'Calgary', 'Edmonton', 'Red Deer', 'Airdrie', 'Johannesburg', 'Manila', 'Singapore', 'Batavia'];
+
+var merchName = ['General Cargo', 'Hotdogs', 'Silk', 'Contraband', 'Books'];
+
+var merchBasePrice = [15, 80, 750, 9000, 27];
+
+var warehouseStock = [200, 15, 5, 1, 157, warehouseCapacity];
+
+var shipStock = [10, 5, 0, 0, 75, shipCapacity, shipGuns, shipHealth];
+
+/* Individual modules */
 
 function display_cities() {
 	var $cityList = $( '#city_list');
+	$cityList.html("");
 	for(var city = 0; city < cities.length; city++) {
 		$cityList.append('<li>' + cities[city] + '</li>');
 	}
@@ -23,14 +36,45 @@ function display_cities() {
 
 function display_merch() {
 	var $priceList = $( '#price_list');
+	$priceList.html("");
 	for(var merch = 0; merch < merchName.length; merch++) {
 		$priceList.append('<li>' + merchName[merch] + ': ' + merchBasePrice[merch] + '</li>');
 	}	
 }
 
+function display_warehouse() {
+	var $warehouseList = $( '#warehouse_list');
+	$warehouseList.html("");
+	var warehouseCargoUsed = 0;
+	for(var merch = 0; merch < merchName.length; merch++) {
+		$warehouseList.append('<li>' + merchName[merch] + ': ' + warehouseStock[merch] + '</li>');
+		warehouseCargoUsed += warehouseStock[merch];
+	}	
+	$warehouseList.append('<li>Capacity: ' + warehouseCargoUsed + '/' + warehouseStock[merchName.length] + '</li>');
+}
+
+function display_ship() {
+	var $shipList = $('#ship_list');
+	$shipList.html("");
+	var shipCargoUsed = 0;
+	for(var merch = 0; merch < merchName.length; merch++) {
+		$shipList.append('<li>' + merchName[merch] + ': ' + shipStock[merch] + '</li>');
+		shipCargoUsed += shipStock[merch];
+	}	
+	$shipList.append('<li>Capacity: ' + shipCargoUsed + '/' + shipStock[merchName.length] + '</li>');
+	$shipList.append('<li>Guns: ' + shipStock[merchName.length + 1] + '</li>');
+	$shipList.append('<li>Health: ' + shipStock[merchName.length + 2] + '</li>');
+}
+
+/* Utility test calls */
+
 function test_utilities() {
 	display_cities();
 	display_merch();
+	display_warehouse();
+	display_ship();
 }
+
+/* Document ready - launch entry */
 
 $(document).ready(readyFn);
