4f9e4841b1cfbee35468b541c1b336b1b0c96331
Rod Weis <rod@codetheweb.ca>
2025-08-22 20:51:24 -0600

    Remove jQuery and refactor to vanilla JS

--- js/taipan.js
+++ js/taipan.js
@@ -1,80 +1,71 @@
 /* Taipan.js */
 
-function readyFn(jQuery) {
-	console.log("jQuery ready");
-	test_utilities();
-}; // /function readyFn
-
 /* Global Data Initialization 
 	TODO:  Import JSON data from source repo
 */
 
-var warehouseCapacity = 10000,
+const warehouseCapacity = 10000,
 	shipCapacity = 100,
-	shipGuns = 5;
+	shipGuns = 5,
 	shipHealth = 100;
 
-var cities = ['Hong Kong', 'Shanghai', 'Nagasaki', 'Calgary', 'Edmonton', 'Red Deer', 'Airdrie', 'Johannesburg', 'Manila', 'Singapore', 'Batavia'];
+const cities = ['Hong Kong', 'Shanghai', 'Nagasaki', 'Calgary', 'Edmonton', 'Red Deer', 'Airdrie', 'Johannesburg', 'Manila', 'Singapore', 'Batavia'];
 
-var merchName = ['General Cargo', 'Hotdogs', 'Silk', 'Contraband', 'Books'];
+const merchName = ['General Cargo', 'Hotdogs', 'Silk', 'Contraband', 'Books'];
 
-var merchBasePrice = [15, 80, 750, 9000, 27];
+const merchBasePrice = [15, 80, 750, 9000, 27];
 
-var warehouseStock = [200, 15, 5, 1, 157, warehouseCapacity];
+let warehouseStock = [200, 15, 5, 1, 157, warehouseCapacity];
 
-var shipStock = [10, 5, 0, 0, 75, shipCapacity, shipGuns, shipHealth];
+let shipStock = [10, 5, 0, 0, 75, shipCapacity, shipGuns, shipHealth];
 
 /* Individual modules */
 
 function display_cities() {
-	var $cityList = $( '#city_list');
-	$cityList.html("");
-	for(var city = 0; city < cities.length; city++) {
-		$cityList.append('<li>' + cities[city] + '</li>');
+	let cityList = document.getElementById('city_list');
+	cityList.innerHTML="";
+	for(let city = 0; city < cities.length; city++) {
+		cityList.innerHTML += ('<li>' + cities[city] + '</li>');
 	}
 }
-
 function display_merch() {
-	var $priceList = $( '#price_list');
-	$priceList.html("");
-	for(var merch = 0; merch < merchName.length; merch++) {
-		$priceList.append('<li>' + merchName[merch] + ': ' + merchBasePrice[merch] + '</li>');
+	let priceList = document.getElementById('price_list');
+	priceList.innerHTML="";
+	for(let merch = 0; merch < merchName.length; merch++) {
+		priceList.innerHTML += ('<li>' + merchName[merch] + ': ' + merchBasePrice[merch] + '</li>');
 	}	
 }
 
 function display_warehouse() {
-	var $warehouseList = $( '#warehouse_list');
-	$warehouseList.html("");
-	var warehouseCargoUsed = 0;
-	for(var merch = 0; merch < merchName.length; merch++) {
-		$warehouseList.append('<li>' + merchName[merch] + ': ' + warehouseStock[merch] + '</li>');
+	let warehouseList = document.getElementById('warehouse_list');
+	warehouseList.innerHTML="";
+	let warehouseCargoUsed = 0;
+	for(let merch = 0; merch < merchName.length; merch++) {
+		warehouseList.innerHTML += ('<li>' + merchName[merch] + ': ' + warehouseStock[merch] + '</li>');
 		warehouseCargoUsed += warehouseStock[merch];
 	}	
-	$warehouseList.append('<li>Capacity: ' + warehouseCargoUsed + '/' + warehouseStock[merchName.length] + '</li>');
+	warehouseList.innerHTML += ('<li>Capacity: ' + warehouseCargoUsed + '/' + warehouseStock[merchName.length] + '</li>');
 }
 
 function display_ship() {
-	var $shipList = $('#ship_list');
-	$shipList.html("");
-	var shipCargoUsed = 0;
-	for(var merch = 0; merch < merchName.length; merch++) {
-		$shipList.append('<li>' + merchName[merch] + ': ' + shipStock[merch] + '</li>');
+	let shipList = document.getElementById('ship_list');
+	shipList.innerHTML="";
+	let shipCargoUsed = 0;
+	for(let merch = 0; merch < merchName.length; merch++) {
+		shipList.innerHTML += ('<li>' + merchName[merch] + ': ' + shipStock[merch] + '</li>');
 		shipCargoUsed += shipStock[merch];
 	}	
-	$shipList.append('<li>Capacity: ' + shipCargoUsed + '/' + shipStock[merchName.length] + '</li>');
-	$shipList.append('<li>Guns: ' + shipStock[merchName.length + 1] + '</li>');
-	$shipList.append('<li>Health: ' + shipStock[merchName.length + 2] + '</li>');
+	shipList.innerHTML += ('<li>Capacity: ' + shipCargoUsed + '/' + shipStock[merchName.length] + '</li>');
+	shipList.innerHTML += ('<li>Guns: ' + shipStock[merchName.length + 1] + '</li>');
+	shipList.innerHTML += ('<li>Health: ' + shipStock[merchName.length + 2] + '</li>');
 }
-
 /* Utility test calls */
 
 function test_utilities() {
-	display_cities();
+	display_cities(); 
 	display_merch();
 	display_warehouse();
 	display_ship();
 }
 
-/* Document ready - launch entry */
-
-$(document).ready(readyFn);
+test_utilities();
